49238b15bf02b7d288d542992339b9b107521da0
Michael Lynch <lyncmi07@Michaels-MBP.lan>
2018-03-29 20:24:42 +0100

    added tests for drawing lines using cursesgraphic

--- termuitests/TermUITests.go
+++ termuitests/TermUITests.go
@@ -11,8 +11,14 @@ func main() {
 }
 
 func mainThread(tui *terminalui.TermUI) {
+	tui.View.SetFillChar('+')
 	tui.View.SetColor(color.Green, color.Black)
 	tui.View.FillRect(0, 0, 100, 40)
 	tui.View.SetColor(color.Red, color.Yellow)
 	tui.View.FillRect(50, 0, 30, 30)
+	tui.View.DrawLine(5, 5, 10, 10)
+	tui.View.DrawLine(10, 5, 5, 10)
+	tui.View.ClearLines()
+	tui.View.DrawLine(10, 10, 10, 20)
+	tui.View.DrawLine(10, 20, 20, 10)
 }
